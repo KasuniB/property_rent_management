51b6906bdd4d302ad46364c1aa6713beda64859a
CodeSandbox <support@codesandbox.io>
2025-04-02 19:57:15 +0000

    fix: Move esbuild.js into app directory and update paths

---
+++ property_rent_management/esbuild.js
@@ -0,0 +1,26 @@
+const esbuild = require('esbuild');
+const path = require('path');
+
+// Build JS
+esbuild.build({
+    entryPoints: ['public/js/index.js'],
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    outfile: 'public/dist/js/property_rent_management.min.js',
+    platform: 'browser',
+    target: ['es2015'],
+    format: 'iife',
+    loader: {
+        '.js': 'jsx'
+    }
+}).catch(() => process.exit(1));
+
+// Build CSS
+esbuild.build({
+    entryPoints: ['public/css/property_rent_management.css'],
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    outfile: 'public/dist/css/property_rent_management.min.css'
+}).catch(() => process.exit(1));
---
+++ property_rent_management/public/js/index.js
@@ -0,0 +1,4 @@
+import './property_rent_management.js';
+
+// This is the main entry point for the app's JavaScript
+console.log('Property Rent Management App initialized');
