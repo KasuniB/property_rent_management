c70062466b5cdc3c31817276f83d3076a00094a9
CodeSandbox <support@codesandbox.io>
2025-04-02 19:53:06 +0000

    feat: Add frontend build configuration and assets

---
+++ property_rent_management/public/js/property_rent_management.js
@@ -0,0 +1,23 @@
+frappe.provide('property_rent_management');
+
+property_rent_management = {
+    init: function() {
+        // Initialize app-specific JavaScript
+    },
+    
+    setup_property_dashboard: function() {
+        // Dashboard initialization
+    },
+    
+    setup_lease_actions: function() {
+        // Lease-related actions
+    },
+    
+    setup_maintenance_handlers: function() {
+        // Maintenance request handlers
+    }
+};
+
+$(document).ready(function() {
+    property_rent_management.init();
+});
