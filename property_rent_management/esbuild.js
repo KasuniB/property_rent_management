b0cac0833512090f73c9dcd093dc237fb89db668
CodeSandbox <support@codesandbox.io>
2025-04-03 01:40:05 +0000

    fix: Update build configuration to work with Frappe paths

--- property_rent_management/esbuild.js
+++ property_rent_management/esbuild.js
@@ -1,22 +1,29 @@
+const path = require('path');
 const esbuild = require('esbuild');
+
+const apps_path = path.resolve(__dirname, '..', '..');
+const assets_path = path.resolve(apps_path, 'property_rent_management', 'public');
 
 // Build JS
 esbuild.build({
-    entryPoints: ['public/js/property_rent_management.js'],
+    entryPoints: [path.resolve(assets_path, 'js/property_rent_management.js')],
     bundle: true,
     minify: true,
     sourcemap: true,
-    outfile: 'public/js/property_rent_management.min.js',
+    outfile: path.resolve(assets_path, 'js/property_rent_management.min.js'),
     platform: 'browser',
     target: ['es2015'],
-    format: 'iife'
+    format: 'iife',
+    define: {
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    }
 }).catch(() => process.exit(1));
 
 // Build CSS
 esbuild.build({
-    entryPoints: ['public/css/property_rent_management.css'],
+    entryPoints: [path.resolve(assets_path, 'css/property_rent_management.css')],
     bundle: true,
     minify: true,
     sourcemap: true,
-    outfile: 'public/css/property_rent_management.min.css'
+    outfile: path.resolve(assets_path, 'css/property_rent_management.min.css')
 }).catch(() => process.exit(1));
