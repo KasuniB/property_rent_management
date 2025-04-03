1ea43d9ef8433df79bb5e894de898844f5c8a381
SajmustafaKe <45385362+SajmustafaKe@users.noreply.github.com>
2025-04-03 05:33:35 +0300

    Update property_rent_management.js

--- property_rent_management/public/js/property_rent_management.js
+++ property_rent_management/public/js/property_rent_management.js
@@ -1,23 +1,27 @@
 frappe.provide('property_rent_management');
 
 property_rent_management = {
-    init: function() {
-        // Initialize app-specific JavaScript
+    init: function () {
+        console.log("[property_rent_management] Initialized");
+
+        this.setup_property_dashboard();
+        this.setup_lease_actions();
+        this.setup_maintenance_handlers();
     },
-    
-    setup_property_dashboard: function() {
-        // Dashboard initialization
+
+    setup_property_dashboard: function () {
+        console.log("Setting up property dashboard...");
     },
-    
-    setup_lease_actions: function() {
-        // Lease-related actions
+
+    setup_lease_actions: function () {
+        console.log("Setting up lease actions...");
     },
-    
-    setup_maintenance_handlers: function() {
-        // Maintenance request handlers
+
+    setup_maintenance_handlers: function () {
+        console.log("Setting up maintenance handlers...");
     }
 };
 
-$(document).ready(function() {
+$(document).ready(function () {
     property_rent_management.init();
 });
